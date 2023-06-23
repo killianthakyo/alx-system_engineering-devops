f2b1a975088cb303d599931ed27e1b26b1bcde70
Killian Nthakyo <killynthakyo@gmail.com>
2023-06-23 11:41:00 -0700

    KIll a process using puppet

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,6 @@
-#KIll a process using Puppet
+#Puppet manifest to kill a process named killmenow
 
-exec { 'killmenow':
-  command => 'pkill killmenow',
-  provider => 'shell,
+exec {'killmenow':
+  command  => 'pkill killmenow',
+  provider => 'shell',
 }
-
