c2b47566538bbb1fea027b661889d1d09da667d0
Killian Nthakyo <killynthakyo@gmail.com>
2023-09-28 02:42:30 -0700

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -15,5 +15,5 @@ if __name__ == "__main__":
     with open(csv_file_name, mode='w', newline='') as csv_file:
         csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
         for t in todos:
-            [csv_writer.writerow(
+            [csv_writer.write(
                     [user_id, username, t.get("completed"), t.get("title")])]
